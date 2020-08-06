016222224844262eca5e26104d4a483e9c20e993
michellezhang95 <michelle.zhang56@gmail.com>
2020-08-06 15:05:54 +1200

    map updates
    created list of points
    loop through each point and create a marker object with new createNewMarker method
    instead of popups bottom sheets will be used

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:coffeehouse_reviews/map/location.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,6 +10,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  static final List<LatLng> _points = [
+    LatLng(-36.7897367, 174.77), // ark company takapuna
+    LatLng(-36.8486, 174.766), //the shelf
+    LatLng(-36.768, 174.737), //toasted coffee roasters
+    LatLng(-36.829, 174.797), //corelli's
+    LatLng(-36.8497, 174.765), // scarecrow
+  ];
+
+  final PopupController _popupLayerController = PopupController();
+
+  List<Marker> _markers;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +29,35 @@ class _HomeState extends State<Home> {
           title: Text('Coffee House Reviews'),
           backgroundColor: Colors.brown[300],
         ),
-        body: new FlutterMap(options: null));
+        body: new FlutterMap(
+          options: new MapOptions(
+            center: new LatLng(-36.817401, 174.743),
+            minZoom: 10.0,
+          ),
+          layers: [
+            new TileLayerOptions(
+                urlTemplate:
+                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                subdomains: ['a', 'b', 'c']),
+            new MarkerLayerOptions(markers: [
+              new Marker(
+                  width: 65.0,
+                  height: 65.0,
+                  point: new LatLng(-36.7897367, 174.77),
+                  builder: (context) => new Container(
+                      child: IconButton(
+                          icon: Icon(Icons.location_on),
+                          color: Colors.brown[500],
+                          iconSize: 50.0,
+                          onPressed: () {
+                            print('tapped');
+                          }))),
+            ]), //specific url for map template
+          ],
+        ));
+  }
+
+  Marker createNewMarker() {
+    //method to create a marker for each latlng point
   }
 }
