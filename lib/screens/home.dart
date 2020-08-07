3b9899099374a41fb4aff5bb480abac3966287b9
michellezhang95 <michelle.zhang56@gmail.com>
2020-08-07 14:41:56 +1200

    bottom sheet updates

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,6 +7,8 @@ import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:coffeehouse_reviews/screens/location_info.dart';
 import 'package:coffeehouse_reviews/models/cafe.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:coffeehouse_reviews/modals/floating_modal.dart';
+import 'package:coffeehouse_reviews/modals/modal_fit.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -14,28 +16,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  static Marker createNewMarker(LatLng coord, String review) {
-    //method to create a marker for each latlng point
-    Marker mk = new Marker(
-        width: 65.0,
-        height: 65.0,
-        point: coord,
-        builder: (context) => new Container(
-            child: IconButton(
-                icon: Icon(Icons.location_on),
-                color: Colors.brown[500],
-                iconSize: 50.0,
-                onPressed: () {
-                  showModalBottomSheet(
-                      context: context,
-                      builder: (builder) {
-                        return Container(
-                            child: Text(review), padding: EdgeInsets.all(40.0));
-                      });
-                })));
-    return mk;
-  }
-
   static final ark = new Cafe(
       address: '123 test street',
       id: 1,
@@ -43,14 +23,22 @@ class _HomeState extends State<Home> {
       review: 'The flat white is great here.',
       location: LatLng(-36.7897367, 174.77));
 
+  static final copper = new Cafe(
+      address: '123 test street',
+      id: 2,
+      name: 'Test Cafe',
+      review: 'The flat white is great here.',
+      location: LatLng(-36.7311, 174.7129));
+
   //static final ark = LatLng(-36.7897367, 174.77);
-  static final copper = LatLng(-36.7311, 174.7129);
+  //static final copper = LatLng(-36.7311, 174.7129);
   static final toasted = LatLng(-36.767, 174.737429);
   static final corellis = LatLng(-36.829, 174.797);
   static final scarecrow = LatLng(-36.8497, 174.765);
   static final Marker markerArk = createNewMarker(ark.location, ark.review);
-  /* markerCopper = createNewMarker(copper),
-      markerToasted = createNewMarker(toasted),
+  static final Marker markerCopper =
+      createNewMarker(copper.location, copper.review);
+  /*  markerToasted = createNewMarker(toasted),
       markerCorellis = createNewMarker(corellis),
       markerScarecrow = createNewMarker(scarecrow); */
 
@@ -58,8 +46,8 @@ class _HomeState extends State<Home> {
 
   List<Marker> _markers = [
     markerArk,
-    /*  markerCopper,
-    markerToasted,
+    markerCopper,
+    /* markerToasted,
     markerCorellis,
     markerScarecrow, */
   ];
@@ -102,3 +90,30 @@ class _HomeState extends State<Home> {
         ));
   }
 }
+
+Marker createNewMarker(LatLng coord, String review) {
+  //method to create a marker for each latlng point
+  Marker mk = new Marker(
+      width: 65.0,
+      height: 65.0,
+      point: coord,
+      builder: (context) => new Container(
+          child: IconButton(
+              icon: Icon(Icons.location_on),
+              color: Colors.brown[500],
+              iconSize: 50.0,
+              onPressed: () {
+                showBottomSheet(
+                    context: context,
+                    builder: (builder) {
+                      return Container(
+                        child: Text(review),
+                        padding: EdgeInsets.all(40.0),
+                      );
+                    });
+              })));
+  return mk;
+}
+
+//method to generate bottom sheets for each new marker on the map
+void _settingBottomSheet(context) {}
